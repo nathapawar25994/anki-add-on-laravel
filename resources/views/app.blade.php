7900883837a780e75736ac8f924b595d35fcb995
akshay2424 <akshayrandhave2424@gmail.com>
2020-05-07 22:16:04 +0530

    fix issue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -99,7 +99,7 @@
         <div class="footer-inner">
             <div class="container">
                 <div class="row">
-                    <div class="span12"> &copy; 2013 <a href="#">Bootstrap Responsive Admin Template</a>. </div>
+                    <div class="span12"> &copy; 2020 <a href="#">Anki web App</a>. </div>
                     <!-- /span12 -->
                 </div>
                 <!-- /row -->
